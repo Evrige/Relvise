07702bc83ecc06cf2717bdfa70972fc963ef0fa9
Artur Dudnik <evrige.game@gmail.com>
2021-12-04 21:36:31 +0200

    ready

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const menuTitle = document.querySelectorAll('.menu__title');
-console.log("hello");
+const menuTitle = document.querySelectorAll('.menu__title'),
+	navLinks = document.querySelectorAll(".navbar__link[data-goto]");
+
+
 menuTitle.forEach((item) =>
 	item.addEventListener('click', function () {
 		item.classList.toggle('_active');
 	}));
 
+if (navLinks.length > 0) {
+	navLinks.forEach(navLink => {
+		navLink.addEventListener("click", (e) => {
+			const navLink = e.target;
+			if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) {
+				const gotoBlock = document.querySelector(navLink.dataset.goto),
+					gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+				window.scrollTo({
+					top: gotoBlockValue,
+					behavior: "smooth"
+				});
+				e.preventDefault;
+			}
+		})
+	})
+}
